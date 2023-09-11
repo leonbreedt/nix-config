4ebf94354f4db661f1960a81369c4c86cf8ae89d
Leon Breedt <leon@sector42.io>
2023-09-11 16:22:04 +1200

    chore: add more environment variables

--- macos/home.nix
+++ macos/home.nix
@@ -4,5 +4,17 @@
 {
   home = {
     inherit stateVersion;
+
+    sessionVariables = {
+      TERM = "xterm-256color";
+      LANG = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      EDITOR = "nvim";
+      PAGER = "bat -p";
+      MANPAGER = "bat -p";
+    };
   };
+
+  programs.fish.enable = true;
 }
