d174a824b92af480155619af53619045f9c4318a
Leon Breedt <leon@sector42.io>
2023-09-11 17:35:04 +1200

    feat: set system.checks.verifyNixPath = false

--- macos/default.nix
+++ macos/default.nix
@@ -3,5 +3,9 @@
 { ... }:
 
 {
+  # Enable the Nix daemon for maintenance activities.
   services.nix-daemon.enable = true;
+
+  # Disable NIX_PATH validation checks, we are using flakes.
+  system.checks.verifyNixPath = false;
 }
