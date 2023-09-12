86f3a5a252826600dcc97309e07368e6766ceaaf
Leon Breedt <leon@sector42.io>
2023-09-12 21:13:28 +1200

    fix: reduce scope

--- macos/home.nix
+++ macos/home.nix
@@ -2,11 +2,9 @@
 { pkgs, configdir, ... }:
 
 {
-  home = {
-    activation = {
-      setRootCaCertificates = ''
-        sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ${configdir}/ssl/certs/sector42-ca.pem
-        '';
-    };
+  home.activation = {
+    setRootCaCertificates = ''
+      sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ${configdir}/ssl/certs/sector42-ca.pem
+    '';
   };
 }
