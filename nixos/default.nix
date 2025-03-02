6b5894ce997066cd49a19386740e4cdf7fc8f743
Leon Breedt <leon@sector42.io>
2025-03-02 22:04:56 +1300

    feat: add Kea DHCP server

--- nixos/default.nix
+++ nixos/default.nix
@@ -108,6 +108,212 @@ in
     };
   };
 
+  services.kea.dhcp4 = {
+    enable = isEdgeRouter;
+    settings = {
+      interfaces-config = {
+        interfaces = [ "enp86s0" ];
+      };
+
+      renew-timer = 86400;
+      rebind-timer = 1800;
+      valid-lifetime = 86400;
+
+      option-data = [
+        {
+          name = "routers";
+          data = "192.168.1.1";
+        }
+        {
+          name = "domain-name-servers";
+          data = "192.168.1.1";
+        }
+        {
+          name = "domain-name";
+          data = "home";
+        }
+        {
+          name = "domain-search";
+          data = "home";
+        }
+      ];
+
+      subnet4 = [
+        {
+          id = 1;
+          subnet = "192.168.1.0/24";
+          pools = [
+            {
+              pool = "192.168.1.160-192.168.1.250";
+            }
+          ];
+          reservations = [
+						# CORE INFRASTRUCTURE
+						{
+							# Infra: Intel NUC
+							hw-address = "b8:ae:ed:71:33:84";
+							ip-address = "192.168.1.1";
+							hostname = "gateway";
+						}
+						{
+							# Infra: Office AP
+							hw-address = "80:2a:a8:c0:88:e7";
+							ip-address = "192.168.1.2";
+							hostname = "office-ap";
+						}
+						{
+							# Infra: Lounge AP
+							hw-address = "80:2a:a8:c3:d1:72";
+							ip-address = "192.168.1.3";
+							hostname = "lounge-ap";
+						}
+						{
+							# Infra: TrueNAS
+							hw-address = "3c:ec:ef:fa:66:26";
+							ip-address = "192.168.1.4";
+							hostname = "nas";
+						}
+						{
+							# Infra: Unifi Controller
+							hw-address = "48:21:b:6a:b5:ee";
+							ip-address = "192.168.1.5";
+							hostname = "unifi-controller";
+						}
+						{
+							# Infra: TP-Link PoE switch
+							hw-address = "50:d4:f7:00:44:35";
+							ip-address = "192.168.1.6";
+							hostname = "tp-link-poe-switch";
+						}
+						{
+							# Infra: CalDigit TS4 Dock (Ethernet)
+							hw-address = "64:4b:f0:37:3a:32";
+							ip-address = "192.168.1.7";
+							hostname = "dock-ethernet";
+						}
+
+						# SERVICES
+				 
+						# Service IPs are not provided via DHCP on TrueNAS SCALE, they are all
+						# statically configured on the NAS interface, since they are not running in Jails
+						# with custom MAC addresses.
+
+						# MACHINES
+
+						{
+							# Machine: Leon's Desktop
+							hw-address = "58:11:22:cf:0c:a3";
+							ip-address = "192.168.1.50";
+							hostname = "leon-desktop";
+						}
+						{
+							# Machine: Leon's MacBook Pro (WiFi)
+							hw-address = "6e:fe:5d:db:c7:ae";
+							ip-address = "192.168.1.51";
+							hostname = "leon-mbp";
+						}
+						{
+							# Machine: Work MacBook Pro (WiFi)
+							hw-address = "3a:20:4c:f3:c1:5c";
+							ip-address = "192.168.1.52";
+							hostname = "leon-desktop";
+						}
+						{
+							# Machine: Chanthaly's MacBook Air
+							hw-address = "f2:64:d8:14:5d:41";
+							ip-address = "192.168.1.53";
+							hostname = "chanthaly-mba";
+						}
+
+						# MOBILE DEVICES
+
+						{
+							# Mobile: Leon's iPhone
+							hw-address = "ee:de:81:78:13:74";
+							ip-address = "192.168.1.100";
+							hostname = "leon-iphone";
+						}
+						{
+							# Mobile: Leon's iPad
+							hw-address = "88:66:5a:8b:32:15";
+							ip-address = "192.168.1.101";
+							hostname = "leon-ipad";
+						}
+						{
+							# Mobile: Leon's Watch
+							hw-address = "a0:d1:b3:f2:e9:e5";
+							ip-address = "192.168.1.102";
+							hostname = "leon-watch";
+						}
+						{
+							# Device: Leon's Tesla Model 3
+							hw-address = "c:29:8f:56:f4:2";
+							ip-address = "192.168.1.103";
+							hostname = "leon-tesla";
+						}
+						{
+							# Mobile: Work iPhone
+							hw-address = "d2:46:1a:f7:a9:eb";
+							ip-address = "192.168.1.104";
+							hostname = "work-iphone";
+						}
+						{
+							# Mobile: Chanthaly's iPhone
+							hw-address = "ca:be:fe:fd:4d:5a";
+							ip-address = "192.168.1.105";
+							hostname = "chanthaly-iphone";
+						}
+						{
+							# Mobile: Chanthaly's Watch
+							hw-address = "6e:17:aa:68:3a:16";
+							ip-address = "192.168.1.106";
+							hostname = "chanthaly-watch";
+						}
+
+						# OTHER DEVICES
+
+						{
+							# Device: Mitsubishi Wifi Control
+							hw-address = "e8:e8:b7:ee:75:ee";
+							ip-address = "192.168.1.140";
+							hostname = "mitsubishi-wifi-control";
+						}
+						{
+							# Device: Tesla Wall Connector
+							hw-address = "98:ed:5c:f7:7a:8a";
+							ip-address = "192.168.1.141";
+							hostname = "tesla-wall-connector";
+						}
+						{
+							# Device: Kitchen HomePod
+							hw-address = "ac:bc:b5:d9:9b:a3";
+							ip-address = "192.168.1.142";
+							hostname = "kitchen-homepod";
+						}
+						{
+							# Device: Lounge TV (Sony Bravia 65")
+							hw-address = "ac:9b:a:2c:f0:55";
+							ip-address = "192.168.1.143";
+							hostname = "lounge-tv";
+						}
+						{
+							# Device: Apple TV (Lounge)
+							hw-address = "f0:b3:ec:1f:8c:4c";
+							ip-address = "192.168.1.144";
+							hostname = "lounge-apple-tv";
+						}
+						{
+							# Device: Yoto Mini
+							hw-address = "d4:8a:fc:ed:49:50";
+							ip-address = "192.168.1.145";
+							hostname = "yoto-mini";
+						}
+          ];
+        }
+      ];
+    };
+  };
+
   services.unbound = {
     enable = isEdgeRouter;
     settings = {
